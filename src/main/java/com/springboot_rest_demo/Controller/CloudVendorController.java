7912ecfeb97e65d7428e9fbc0c59b45f4ce121c1
Palledar Praveen <praveenkumar.p347@gmail.com>
2024-06-06 14:51:30 +0530

    Swagger changes for providing the description

--- src/main/java/com/springboot_rest_demo/Controller/CloudVendorController.java
+++ src/main/java/com/springboot_rest_demo/Controller/CloudVendorController.java
@@ -17,6 +17,8 @@ import com.springboot_rest_demo.Service.CloudVendorService;
 import com.springboot_rest_demo.model.CloudVendor;
 import com.springboot_rest_demo.response.ResponseHandler;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping("/cloudvendor")
 public class CloudVendorController {
@@ -30,6 +32,7 @@ public class CloudVendorController {
 
 	// Read Specific Vendor id details Present in DB
 	@GetMapping("{vendorId}")
+	@Operation(description = "To get particular vendor details by passing vendorId")
 	public ResponseEntity<Object> getCloudVendorDetails(@PathVariable ("vendorId") String vendorId) { 
 		
 		return ResponseHandler.responseBuilder("Requested CloudVendor details are given here", 
@@ -44,24 +47,28 @@ public class CloudVendorController {
 	 */
 	// Read All the Vendor id details present in DB
 	@GetMapping()
+	@Operation(description = "To get all vendor details present in DB")
 	public List<CloudVendor> getAllCloudVendorDetails() { 
 		return cloudVendorService.getAllCloudVendors();
 	
 	};
 	
 	@PostMapping
+	@Operation(description = "Add new vendor details by passing the Json object")
 	public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
 		cloudVendorService.createCloudVendor(cloudVendor);
 		return "Successfully added the vendor details of "+ cloudVendor;
 	}
 
 	@PutMapping
+	@Operation(description = "Update the requested vendor details by passing the Json object")
 	public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
 		cloudVendorService.updateCloudVendor(cloudVendor);
 		return "Successfully updated the vendor details ";
 	}
 	
 	@DeleteMapping("{vendorId}")
+	@Operation(description = "Deleting the vendor details by passing vendorId")
 	public String deleteCloudVendorDetails(@PathVariable ("vendorId") String vendorId) {
 		cloudVendorService.deleteCloudVendor(vendorId);
 		return "Successfully deleted the vendor details ";
